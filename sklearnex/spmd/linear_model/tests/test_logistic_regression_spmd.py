6b167567da1f8ad2c1ee23c70df20ed93d90f046
Anatoly Volkov <anatoly.volkov@intel.com>
2024-09-18 11:09:53 -0700

    Remove redundant outputs

--- sklearnex/spmd/linear_model/tests/test_logistic_regression_spmd.py
+++ sklearnex/spmd/linear_model/tests/test_logistic_regression_spmd.py
@@ -29,8 +29,8 @@ from sklearnex.tests._utils_spmd import (
     _mpi_libs_and_gpu_available,
     _spmd_assert_allclose,
 )
-import sys
-'''
+
+
 @pytest.mark.skipif(
     not _mpi_libs_and_gpu_available,
     reason="GPU device and MPI libs required for test",
@@ -98,41 +98,31 @@ def test_logistic_spmd_gold(dataframe, queue):
     batch_result = batch_model.predict(dpt_X_test)
 
     _spmd_assert_allclose(spmd_result, _as_numpy(batch_result))
-'''
+
 
 # parametrize max_iter, C, tol
 @pytest.mark.skipif(
     not _mpi_libs_and_gpu_available,
     reason="GPU device and MPI libs required for test",
 )
-@pytest.mark.parametrize("n_samples", [10000])
-@pytest.mark.parametrize("n_features", [100])
-@pytest.mark.parametrize("C", [0.5])
-@pytest.mark.parametrize("tol", [1e-2])
+@pytest.mark.parametrize("n_samples", [100, 10000])
+@pytest.mark.parametrize("n_features", [10, 100])
+@pytest.mark.parametrize("C", [0.5, 1.0, 2.0])
+@pytest.mark.parametrize("tol", [1e-2, 1e-4])
 @pytest.mark.parametrize(
     "dataframe,queue",
-    get_dataframes_and_queues(dataframe_filter_="dpctl", device_filter_="gpu"),
+    get_dataframes_and_queues(dataframe_filter_="dpnp,dpctl", device_filter_="gpu"),
 )
-@pytest.mark.parametrize("dtype", [np.float64])
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
 @pytest.mark.mpi
 def test_logistic_spmd_synthetic(n_samples, n_features, C, tol, dataframe, queue, dtype):
     # TODO: Resolve numerical issues when n_rows_rank < n_cols
     if n_samples <= n_features:
         pytest.skip("Numerical issues when rank rows < columns")
 
-    print(n_samples, n_features, C, tol, dataframe, queue, dtype)
-    sys.stdout.flush()
-
-
-    from mpi4py import MPI
- 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-
     # Import spmd and batch algo
     from sklearnex.linear_model import LogisticRegression as LogisticRegression_Batch
     from sklearnex.spmd.linear_model import LogisticRegression as LogisticRegression_SPMD
-    from sklearn.metrics import log_loss
 
     # Generate data and convert to dataframe
     X_train, X_test, y_train, _ = _generate_classification_data(
@@ -153,57 +143,15 @@ def test_logistic_spmd_synthetic(n_samples, n_features, C, tol, dataframe, queue
     dpt_X_test = _convert_to_dataframe(X_test, sycl_queue=queue, target_df=dataframe)
 
     # Ensure trained model of batch algo matches spmd
-    print("Data shapes:", X_train.shape, y_train.shape, dpt_X_train.shape, local_dpt_X_train.shape)
-    print("Data sum:", X_train.sum(), y_train.sum())
-    print("Row sums:")
-    print(X_train.sum(axis=0))
-    print(X_train.sum(axis=1))
-    sys.stdout.flush()
-    print("First 10 rows")
-    for i in range(10):
-        for j in range (X_train.shape[1]):
-            print(X_train[i, j], end=' ')
-        print()
-    print("---------------------")
-    print("Last 10 rows")
-    for i in range(X_train.shape[0] - 10, X_train.shape[0]):
-        for j in range (X_train.shape[1]):
-            print(X_train[i, j], end=' ')
-        print()
-    print("---------------------")
-    sys.stdout.flush()
-    
     spmd_model = LogisticRegression_SPMD(
         random_state=0, solver="newton-cg", C=C, tol=tol
     ).fit(local_dpt_X_train, local_dpt_y_train)
-    
     batch_model = LogisticRegression_Batch(
         random_state=0, solver="newton-cg", C=C, tol=tol
     ).fit(dpt_X_train, dpt_y_train)
-    
+
     # TODO: Logistic Regression coefficients do not align
     tol = 1e-2
-
-    print("Spmd coef:", spmd_model.coef_)
-    print(spmd_model.intercept_)
-    print("Batch coef:", batch_model.coef_)
-    print(batch_model.intercept_)
-    sys.stdout.flush()
-
-    spmd_loss = log_loss(_as_numpy(local_dpt_y_train), _as_numpy(spmd_model.predict_proba(local_dpt_X_train)))
-    batch_loss = log_loss(y_train, _as_numpy(batch_model.predict_proba(dpt_X_train)))
-
-    print("Rank:", rank, "spmd loss:", spmd_loss,  spmd_loss * local_dpt_y_train.shape[0])
-    print("Batch loss", batch_loss, batch_loss * y_train.shape[0])
-    sys.stdout.flush()
-
-    spmd_reg = (spmd_model.coef_ ** 2).sum() / (2 * C)
-    batch_reg = (batch_model.coef_ ** 2).sum() / (2 * C)
-
-    print("Rank:", rank, "spmd regul:", spmd_reg, "batch regul:", batch_reg)
-
-    sys.stdout.flush()
-
     assert_allclose(spmd_model.coef_, batch_model.coef_, rtol=tol, atol=tol)
     assert_allclose(spmd_model.intercept_, batch_model.intercept_, rtol=tol, atol=tol)
 
@@ -212,6 +160,3 @@ def test_logistic_spmd_synthetic(n_samples, n_features, C, tol, dataframe, queue
     batch_result = batch_model.predict(dpt_X_test)
 
     _spmd_assert_allclose(spmd_result, _as_numpy(batch_result))
-
-    # dummy assert
-    assert False
